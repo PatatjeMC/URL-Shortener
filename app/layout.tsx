944a541c8c84cfd42516bdf9172cc4a111ef1d89
PatatjeMC <patatje@razeempire.net>
2024-08-06 15:28:34 +0200

    Added shadcn/ui and setup darkmode switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					{children}
+					<div className="absolute top-5 right-5">
+						<ThemeSwitcher />
+					</div>
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
