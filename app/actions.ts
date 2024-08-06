1888d55d180de1fddc48f219c7d8ac8aefed7d5c
PatatjeMC <patatje@razeempire.net>
2024-08-06 17:31:03 +0200

    Made the create url function functional.
    The function now works but it isn't finished yet. But it is now testable. Also needed to change a few small things in the database scheme for this.

--- app/actions.ts
+++ app/actions.ts
@@ -1,19 +1,35 @@
 "use server";
 
 import { PrismaClient } from "@prisma/client";
+import { randomInt } from "crypto";
 import Sqids from "sqids";
 
 const prisma = new PrismaClient();
-const sqids = new Sqids();
+const sqids = new Sqids({
+    alphabet: 'pOYJGFiqugAHXIPjnmTMc8095e74dQNft2wUEZbxa1hBl6RkLKVCWsryz3voSD',
+    minLength: 8,
+});
 
 export async function create(url: string) {
     const shortenedUrl = await prisma.url.create({
         data: {
             fullUrl: url,
-            id: sqids.encode([1, 2, 3]),
             createdBy: "test", // Change this to IP addres later.
         },
     });
 
-    return shortenedUrl;
+    const id = shortenedUrl.id;
+
+    const shortUrl = sqids.encode([id, url.length])
+
+    await prisma.url.update({
+        where: {
+            id,
+        },
+        data: {
+            shortUrl,
+        }
+    })
+
+    return shortUrl;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,32 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { create } from "./actions";
+import { FormEvent } from "react";
 
 export default function Home() {
 	return (
 		<div className="flex justify-center items-center h-screen w-screen">
-			<div className="w-[40%] flex h-14">
+			<form onSubmit={onSubmit} className="w-[40%] flex h-14">
 				<Input
+					name="url"
 					placeholder="Enter URL to shorten!"
 					className="h-full rounded-r-none"
 				></Input>
 				<Button variant={"default"} className="h-full rounded-l-none">
 					Shorten
 				</Button>
-			</div>
+			</form>
 		</div>
 	);
+}
+
+function onSubmit(event: FormEvent<HTMLFormElement>) {
+	event.preventDefault();
+
+	const formData = new FormData(event.currentTarget);
+	create(formData.get("url") as string).then((data) => {
+		console.log(data);
+	});
 }
