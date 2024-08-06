48b5f808a293951d4b5ad63a5d3ef0482df01626
PatatjeMC <patatje@razeempire.net>
2024-08-06 16:29:21 +0200

    Setup a database
    I created the database schema and setup the database using Prisma ORM to easily manage and use the database. I also updated the readme to exclude some private files.

---
+++ prisma/migrations/20240806142647_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Url" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fullUrl" TEXT NOT NULL,
+    "createdBy" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
