5b712fb390e1ab594b1145440a3fc05173172f32
PatatjeMC <patatje@razeempire.net>
2024-08-06 15:50:10 +0200

    Started working on the ui
    Started working on a basic homepage by adding some of the required ui elements and positioning them. Also generated a custom shadcn/ui theme using https://zippystarter.com/tools/shadcn-ui-theme-generator

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
-	return <></>;
+	return (
+		<div className="flex justify-center items-center h-screen w-screen">
+			<div className="w-[40%] flex h-14">
+				<Input
+					placeholder="Enter URL to shorten!"
+					className="h-full rounded-r-none"
+				></Input>
+				<Button variant={"default"} className="h-full rounded-l-none">
+					Shorten
+				</Button>
+			</div>
+		</div>
+	);
 }
