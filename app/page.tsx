a8f739bc8bc6f1c168379afbdfd1790a3015aa54
PatatjeMC <patatje@razeempire.net>
2024-08-07 18:07:04 +0200

    Added loading animation

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,22 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { create } from "./actions";
 import { FormEvent, useState } from "react";
+import { Loader2 } from "lucide-react";
 
 export default function Page() {
 	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	function onSubmit(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		setLoading(true);
+
+		const formData = new FormData(event.currentTarget);
+		create(formData.get("url") as string).then((data) => {
+			console.log(data);
+			setLoading(false);
+		});
+	}
 
 	return (
 		<>
@@ -16,9 +29,11 @@ export default function Page() {
 						name="url"
 						placeholder="Enter URL to shorten!"
 						className="h-full rounded-r-none"
+						disabled={loading}
 					></Input>
-					<Button variant={"default"} className="h-full rounded-l-none">
-						Shorten
+					<Button variant={"default"} className="h-full rounded-l-none w-24">
+						{!loading && "Shorten"}
+						{loading && <Loader2 className="animate-spin"></Loader2>}
 					</Button>
 				</form>
 			</div>
@@ -29,13 +44,4 @@ export default function Page() {
 			)}
 		</>
 	);
-}
-
-function onSubmit(event: FormEvent<HTMLFormElement>) {
-	event.preventDefault();
-
-	const formData = new FormData(event.currentTarget);
-	create(formData.get("url") as string).then((data) => {
-		console.log(data);
-	});
 }
